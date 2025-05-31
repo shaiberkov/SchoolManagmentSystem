5a8eead13db91b50bd85150147cf4473b3540e63
shai berkovich <shaiberkov@gmail.com>
2025-05-31 22:42:31 +0300

    עיצוב טיילוינד מושקע לכל הקומפוננטות

--- src/constants/base.constants.js
+++ src/constants/base.constants.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = 'http://localhost:8080'; // או מה שבחרת
+export const API_BASE_URL = 'https://learning-project-server.onrender.com';
 export const BASE_API = '/Learning-App';
