3868c825e881a5a3a5878c4f3783a080c7401f79
shai berkovich <shaiberkov@gmail.com>
2025-06-11 15:43:50 +0300

    לוגו לאתר

--- src/constants/base.constants.js
+++ src/constants/base.constants.js
@@ -1,4 +1,4 @@
-// export const API_BASE_URL = 'https://learning-project-server.onrender.com';
- export const API_BASE_URL = 'http://localhost:8080';
+ export const API_BASE_URL = 'https://learning-project-server.onrender.com';
+// export const API_BASE_URL = 'http://localhost:8080';
 
 export const BASE_API = '/Learning-App';
